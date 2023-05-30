f9e6d9433c10f90650b7991aa0162c5a8aa9c5df
Manivannan S <sharvamanivannan@gmail.com>
2023-05-30 08:07:28 +0000

    Routes, Controllers, Models

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
+import connectDB from './db.js'
 import * as dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import cors from "cors";
+import { Keto } from "./Routes/Keto.js";
 
 const app = express();
 const PORT = 8000;
@@ -10,13 +12,27 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
+//connect to database
+connectDB();
+
 const MONGO_URL = process.env.MONGO_URL;
+//MongoDB connection
+async function createConnection() {
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log("MongoDb is connected");
+    return client;
+}
+
+export const client = await createConnection();
 
 app.get("/", (req, res) => {
     res.send("Diet_Suggestion App Backend is Working");
 });
 
+app.use("/keto", Keto);
+
 
 app.listen(PORT, () =>
-    console.log(`Server started on PORT:${PORT}`)
+    console.log(`https://localhost:${PORT}`)
 );
